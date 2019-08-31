1575c6e5cc81531df6d6f96be139cc2fc2d93e57
monserrat <monserrat@kodemia.mx>
2019-08-31 17:50:12 -0500

    estilos

--- js/main.js
+++ js/main.js
@@ -41,37 +41,37 @@ var arrayMovies = [{
     indice1:{
         cover: 'img/traicioncartel.jpg',
         sinopsis: 'Las fracturas familiares entre Félix y su hija Misela, forjadas incluso antes de su nacimiento. El temperamento impetuoso de ambos provoca que los rencores del pasado y su travesía personal a lo largo de atropelladas vidas moldeé una enfermiza y latente sospecha mutua, '+
-        'la cual explota en una tarde nublada mientras lod dos dan un paseo por un terreno roco.',
-        youtube: ''
+        'la cual explota en una tarde nublada mientras los dos dan un paseo por un terreno roco.',
+        youtube: 'https://www.youtube.com/watch?v=0t1hjyeyxqg'
     },
     indice2:{
         cover: 'img/elañodelaplagacartel.jpg',
         sinopsis: 'Tras romper con Irene, los amigos de Victor se empeñan en encontrarle una novia, pero él no quiere olvidarla, incluso cuando aparece Lola. Súbitamente, Irene lo llama, nerviosa, y le pide ayuda porque la gente a su alrededor se comporta de modo diferente, como si hubieran '+
         'perdido sus recuerdos y emociones. Victor decide convertirse en el héroe que nunca fue para Irene y salir en su ayuda.',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=WBH7N1W-2jc'
     },
     indice3:{
         cover: 'img/feralcartel.jpg',
         sinopsis: 'En 1986, un sacerdotem Juan Felipe de Jesús (Héctor Illanes) documenta en docenas de videocassettes el duro proceso para reincorporar a tres niños salvajes a la sociedad oaxaqueña.' +
             'Sus buenas intenciones son interrumpidas por un trágico incendio que marca el destino de Juan Felipe y de los tres pequeños. Hoy en día, varias incógnitas rodean el caso, y los pobladores que vivieron el suceso guardar un misterio silcencio. (FILMAFFINITY).',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=L9jRHTiyfHk&t=20s'
     },
     indice4:{
         cover: 'img/malacopacartel.jpg',
         sinopsis: 'Un hombre tímido y con pocas habilidades sociales se enfrenta a un gran reto en su trabajo cuando le piden que lidere una importante presentación, pero su mundo está a punto de cambiar gracias a la aparición de su alter ego alcoholizado.',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=9nI0nD1TtS0'
     },
     indice5:{
         cover: 'img/influenciacartel.jpg',
         sinopsis: 'Leonora una chica de 16 años, vive con su abuela en un cuarto de azotea aislada del mundo exterior desde hace 10 años, pues está convencida de que tiene el don de propiciar la muerte de gente a las que le ve el rostro. El mágico mundo que ha creado para sobrevivir'+
         ' el encierro se cimbra cuando su abuela muere y conoce a Brandon, un urbano principe a quien oculta de la mafia del barrio y por quien conocerá el Amor y será liberada de su fatídico don, no sin pagar un alto precio.',
-        youtube: ''
+        youtube: 'http://www.influenciathemovie.com/'
     },
     indice6:{
         cover: 'img/elbailedelagacelacartel.jpg',
         sinopsis: 'El sueño de Eugenio es ganar un trofeo, pero nunca lo logró en el fútbol. A sus 72 años encuentra una última posibilidad en un concurso de bailes tropicales. Cuando puerde a su pareja, su única opción de ganar será bailar con otro hombre, enfrentándose a sus propios'+
         ' prejuicios y a los de su familia. Ahora cambiará el balón por el salón.',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=2oo1BN4ulAg&t=21s'
     },
     indice7:{
         cover: 'img/corajecartel.jpg',
@@ -82,14 +82,14 @@ var arrayMovies = [{
         cover: 'img/alcaerlanochecartel.jpg',
         sinopsis: 'Basado en hechos reales. Ángel, un universitario que no gusta de meterse en problemasm termina envuelto con sus vecinos en el linchamiento de un tipo al que los gritos de auxilio de una adolescente, acusan de haberla violado. Jorge, un policía ministerial, vuelve al'+
         ' trabajo tras tiempo de ausencia por una pérdida personal. Él y un veterano policía, tras atender la emergencia, acaban llevándose a un lugar desolado a Ángel y a uno de sus vecinos, junto al hombre que lincharon. Ahí, los presionan a terminar lo que empezaron.',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=pCzdfCSs8II&t=19s'
     },
     indice9:{
         cover: 'img/lavenjanzadejairocartel.jpg',
         sinopsis: 'Jairo José Panilla Téllez (Cali, Colombia, 1944) es el padre del terror el suspenso y la ciencia ficción en Colombia, director entre otras, de películas como 27 horas con la muerte o Funeral Sinestro, que fueron un ícono y un éxito de taquilla por allá por los años 70 y 80 '+
         'Fue el primero en introducir efectos especiales en una película colombiana y ahora, con más de 70 años, está empeñado en sacar adelante su última película: la primera en 3D por fuera de la animación. Llevará por título El espiritu de la muerte. Él mismo, en su casa, se encarga de todos estos efectos, '+
         'trabajándolos, muchas veces de forma astesanal en el proceso de edición. Con entrevistas y fragmentos de películas, se arma este entrañable documental que sigue los pasos de quien, sin duda, es un personake en la historia del cine colombiano, cuya labor bien merece este homenaje.',
-        youtube: ''
+        youtube: 'https://www.youtube.com/watch?v=SaUKsOgbr_I'
     },
 }]
 
@@ -100,7 +100,7 @@ function fillModal(indice){
     $(".trailer").addClass("d-inline-flex")
     $(".img-cartel").attr("src", arrayMovies[0][indice].cover)
     $(".sinopsis-cartel").text(arrayMovies[0][indice].sinopsis)
-    $(".youtube").attr("src", arrayMovies[0][indice].youtube)
+    $(".youtube").attr("href", arrayMovies[0][indice].youtube)
 
     if(indice == 'indice7'){
         $(".title-cartel, .trailer").addClass("d-none")
@@ -108,4 +108,17 @@ function fillModal(indice){
         $(".trailer").removeClass("d-inline-flex")
     }
     $("#cartel-modal").modal("show")
+}
+
+function showModal(){
+    $("#about-us-modal").modal("show")
+    setTimeout(function(){
+        $("#about-us-modal").find(".move-left").animate({
+            marginLeft: '0px'
+        }, 'slow')
+        $("#about-us-modal").find(".move-right").animate({
+            marginRight: '0px'
+        }, 'slow')
+    }, 800)
+    
 }
